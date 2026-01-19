20ff30bf0b468fed59f5cfff1241303aa1853ce5
sintaaaaaz <sintaua20@gmail.com>
2026-01-19 11:45:24 +0700

    perbaikin sikit sikit

--- auth/login.php
+++ auth/login.php
@@ -56,7 +56,7 @@ $error = '';
             <button type="submit" class="btn-login">Masuk ke Sistem</button>
             <div style="margin-top: 20px; text-align: center; font-size: 14px; color: #666;">
     Belum memiliki akun? 
-    <a href="../../auth/register_mahasiswa.php" style="color: #059669; font-weight: 600; text-decoration: none;">Sign In Disini</a>
+    <a href="register_mahasiswa.php" style="color: #059669; font-weight: 600; text-decoration: none;">Sign In Disini</a>
 </div>
         </form>
     </div>
